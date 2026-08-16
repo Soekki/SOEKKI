7f5e7e8dcd4e64e81313134004ae4b2c5305641e
Soekki <extratntkool@gmail.com>
2026-08-16 23:46:36 +0400

    Update ViolenceDistrict.lua

--- ViolenceDistrict.lua
+++ ViolenceDistrict.lua
@@ -3,13 +3,13 @@ print("[SOEKKI] Loading Violence District...")
 
 local success, result = pcall(function()
     -- Загружаем функции ESP
-    local functions = loadstring(game:HttpGet("https://raw.githubusercontent.com/Soekki/SOEKKI/refs/heads/main/main_functions.lua"))()
+    local functions = loadstring(game:HttpGet("https://raw.githubusercontent.com/Soekki/ViolenceDistrict/refs/heads/main/main_functions.lua"))()
     if not functions then
         error("Failed to load main_functions.lua")
     end
     
     -- Загружаем UI
-    local ui = loadstring(game:HttpGet("https://raw.githubusercontent.com/Soekki/SOEKKI/refs/heads/main/main_ui.lua"))()
+    local ui = loadstring(game:HttpGet("https://raw.githubusercontent.com/Soekki/ViolenceDistrict/refs/heads/main/main_ui.lua"))()
     if not ui then
         error("Failed to load main_ui.lua")
     end
